3afcf4ac36c42247279bddc8b0fd6edd4ec78660
Francis Ndungu <ndunguf699@gmail.com>
2025-08-12 01:24:54 +0300

    FIFA Player Cards Project

---
+++ src/players.js
@@ -0,0 +1,36 @@
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Inter Miami",
+    nationality: "Argentina",
+    jerseyNumber: 10,
+    age: 36,
+    imageUrl: "https://i.imgur.com/fh7FghF.png",
+  },
+  {
+    name: "Cristiano Ronaldo",
+    team: "Al Nassr",
+    nationality: "Portugal",
+    jerseyNumber: 7,
+    age: 39,
+    imageUrl: "https://i.imgur.com/h8VdxW2.png",
+  },
+  {
+    name: "Kylian Mbappé",
+    team: "Paris Saint-Germain",
+    nationality: "France",
+    jerseyNumber: 7,
+    age: 25,
+    imageUrl: "https://i.imgur.com/6v3QnX7.png",
+  },
+  {
+    name: "Erling Haaland",
+    team: "Manchester City",
+    nationality: "Norway",
+    jerseyNumber: 9,
+    age: 24,
+    imageUrl: "https://i.imgur.com/OLzxJYV.png",
+  },
+];
+
+export default players;
